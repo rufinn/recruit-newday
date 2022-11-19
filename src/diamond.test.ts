da3a3caaf39d20c1cd39184f613f38950a78d269
rufinatsoi <2770030+rufinn@users.noreply.github.com>
2022-11-19 13:08:31 +0000

    update tests

--- src/diamond.test.ts
+++ src/diamond.test.ts
@@ -2,6 +2,9 @@ const diamond = require('./diamond');
 
 describe('Diamond Kamta', () => {
     const spyConsole = jest.spyOn(console, 'log');
+    beforeEach(() => {
+        spyConsole.mockClear();
+    });
 
     it('output should be A if input character is A', () => {
         diamond.printDiamond('A');
@@ -12,31 +15,23 @@ describe('Diamond Kamta', () => {
     it('output should be a if input character is a', () => {
         diamond.printDiamond('a');
         expect(spyConsole).toHaveBeenCalledTimes(1);
-        expect(spyConsole).toHaveBeenCalledWith('A');
+        expect(spyConsole).toHaveBeenCalledWith('a');
     });
     
     it('input charter in upper case should be the midpoint of the diamond', () => {
         diamond.printDiamond('C');
-        expect(spyConsole).toHaveBeenCalledTimes(7);
-        expect(spyConsole).toHaveBeenCalledWith([
-            '   A . ',
-            '  B B  ',
-            ' C.  C ',
-            '  B B  ',
-            '   A   ',
-        ]);
+        expect(spyConsole).toHaveBeenCalledTimes(1);
+        expect(spyConsole).toHaveBeenCalledWith(
+            `__A__\n_B_B_\nC___C\n_B_B_\n__A__`
+        );
     });
 
     it('input charter in lower case should be the midpoint of the diamond', () => {
-        diamond.printDiamond('C');
-        expect(spyConsole).toHaveBeenCalledTimes(7);
-        expect(spyConsole).toHaveBeenCalledWith([
-            '   a . ',
-            '  b b  ',
-            ' c.  c ',
-            '  b b  ',
-            '   a   ',
-        ]);
+        diamond.printDiamond('c');
+        expect(spyConsole).toHaveBeenCalledTimes(1);
+        expect(spyConsole).toHaveBeenCalledWith(
+            `__a__\n_b_b_\nc___c\n_b_b_\n__a__`
+        );
     });
     
     it('should render error message if input has more than one character', () => {
